03233240ff59d480dfe59d725b54d1d877f956c4
kimshun0213kr <96727208+kimshun0213kr@users.noreply.github.com>
2025-06-20 02:42:44 +0900

    Update: グラフの修正

--- 輝度と視覚の関係/01.py
+++ 輝度と視覚の関係/01.py
@@ -16,7 +16,7 @@ for i in range(maxVal+1):
     y100_3 = math.pow(y100,(1/3))
     y100_3_116 = 116*y100_3
     Y.append(y*255)
-    L.append(((y100_3_116-16)/100)*255)
+    L.append(y100_3_116-16)
 
     
 ax.plot(Y,L)
@@ -27,7 +27,7 @@ ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 
 ax.set_xlim([0, 255]) 
-ax.set_ylim([0, 255]) 
+ax.set_ylim([0, 100]) 
 
 ax.set_xlabel("Actual brightness(DMX Value)")
 ax.set_ylabel("Brightness perceived by the sense of sight")
@@ -35,7 +35,7 @@ ax.set_ylabel("Brightness perceived by the sense of sight")
 equation_text = r'$L^*  = 116(\frac{Y}{255})^{\frac{1}{3}}-16$'
 
 text_x = 0.7*255
-text_y = 0.2*255
+text_y = 0.2*100
 
 ax.text(text_x, text_y, equation_text, fontsize=12,
         ha='center', va='center',
